7e4960c5e6d4b233f8cde516ccd9903164d12dbc
alllvp <alllvp@gmail.com>
2022-11-27 16:35:30 +0300

    new with Dockerfile and Docker-compose

--- README.md
+++
@@ -1,7 +0,0 @@
-**Web-application — task manager.**
-
-_will allow you to work with goals and track progress on them_
-
-stack (python3.10, Django, Postgres)
-
-**how to install:**
--- todolist/settings.py
+++ todolist/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': env.str('POSTGRES_DB'),
         'USER': env.str('POSTGRES_USER'),
         'PASSWORD': env.str('POSTGRES_PASSWORD'),
-        'HOST': env.str('POSTGRES_HOST', 'localhost'),
+        'HOST': env.str('POSTGRES_HOST', default='127.0.0.1'),
         'PORT': env.str('POSTGRES_PORT'),
 
     }
